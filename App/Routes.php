8a0dcbb3917c473ffbb152170e88befbd6b82da0
David <davidign1688@hotmail.com>
2018-06-13 21:28:20 +0200

    Añadimos las rutas de reloj y contact

--- App/Routes.php
+++ App/Routes.php
@@ -3,4 +3,6 @@
 $app->group('', function(){
 	$this->get('/home','ProjectController:home')->setName('home');
 	$this->get('/about','ProjectController:about')->setName('about');
+	$this->get('/reloj','ProjectController:reloj')->setName('reloj');
+		$this->get('/contact','ProjectController:contact')->setName('contact');
 });
