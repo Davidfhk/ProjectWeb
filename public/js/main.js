2da5444f1f88237816180e14d27f47bf9ce3c8bf
David <davidign1688@hotmail.com>
2018-06-13 20:32:49 +0200

    Añadimos en nuestro main.js condicionales para las diferentes templates. Y utilizamos el metodo accordion de jquery UI al div con el id accordion

--- public/js/main.js
+++ public/js/main.js
@@ -2,47 +2,50 @@
 
 $(document).ready(function(){
 
+	if(window.location.href.indexOf('/home') > -1)
+	{
 
 // Slider
 
-	$('.bxslider').bxSlider({
-		mode: 'fade',
-		captions: true,
-		slideWidth: 1200
-	});
+		$('.bxslider').bxSlider({
+			mode: 'fade',
+			captions: true,
+			slideWidth: 1200
+		});
 
 
-// Posts
+	// Posts
 
-	var posts = [
-		{
-			"title":"Primer Post",
-			"date": "Publicado el " + moment().date() + " de "+ moment().format('MMMM') + " del "+ moment().format('YYYY'),
-			"content": "Etiam ultricies libero id porttitor ornare. Fusce hendrerit, dolor in efficitur euismod, dui lorem convallis quam, ac condimentum velit mauris vitae lectus. Vestibulum venenatis lacinia ante, id congue eros tincidunt in. Curabitur molestie nec augue non molestie. Fusce laoreet risus sit amet commodo fringilla. Pellentesque dapibus lorem et dolor tristique, id pulvinar urna lacinia. Pellentesque egestas neque vel arcu volutpat, malesuada hendrerit turpis semper. Sed vulputate purus nec rutrum pellentesque. Fusce ut tincidunt nulla, sed finibus turpis. Proin feugiat pulvinar ornare. Nulla ut sem ut ligula finibus pharetra. In lobortis ligula ac laoreet gravida. Nam vel orci lectus. Suspendisse potenti. Cras at nibh fringilla, vehicula felis sit amet, euismod augue. Vestibulum id diam et arcu suscipit pharetra non sit amet ex."
-		},
-		{
-			"title":"Segundo Post",
-			"date": "Publicado el " + moment().date() + " de "+ moment().format('MMMM') + " del "+ moment().format('YYYY'),
-			"content": "Etiam ultricies libero id porttitor ornare. Fusce hendrerit, dolor in efficitur euismod, dui lorem convallis quam, ac condimentum velit mauris vitae lectus. Vestibulum venenatis lacinia ante, id congue eros tincidunt in. Curabitur molestie nec augue non molestie. Fusce laoreet risus sit amet commodo fringilla. Pellentesque dapibus lorem et dolor tristique, id pulvinar urna lacinia. Pellentesque egestas neque vel arcu volutpat, malesuada hendrerit turpis semper. Sed vulputate purus nec rutrum pellentesque. Fusce ut tincidunt nulla, sed finibus turpis. Proin feugiat pulvinar ornare. Nulla ut sem ut ligula finibus pharetra. In lobortis ligula ac laoreet gravida. Nam vel orci lectus. Suspendisse potenti. Cras at nibh fringilla, vehicula felis sit amet, euismod augue. Vestibulum id diam et arcu suscipit pharetra non sit amet ex."
-		},
-		{
-			"title":"Tercer Post",
-			"date": "Publicado el " + moment().date() + " de "+ moment().format('MMMM') + " del "+ moment().format('YYYY'),
-			"content": "Etiam ultricies libero id porttitor ornare. Fusce hendrerit, dolor in efficitur euismod, dui lorem convallis quam, ac condimentum velit mauris vitae lectus. Vestibulum venenatis lacinia ante, id congue eros tincidunt in. Curabitur molestie nec augue non molestie. Fusce laoreet risus sit amet commodo fringilla. Pellentesque dapibus lorem et dolor tristique, id pulvinar urna lacinia. Pellentesque egestas neque vel arcu volutpat, malesuada hendrerit turpis semper. Sed vulputate purus nec rutrum pellentesque. Fusce ut tincidunt nulla, sed finibus turpis. Proin feugiat pulvinar ornare. Nulla ut sem ut ligula finibus pharetra. In lobortis ligula ac laoreet gravida. Nam vel orci lectus. Suspendisse potenti. Cras at nibh fringilla, vehicula felis sit amet, euismod augue. Vestibulum id diam et arcu suscipit pharetra non sit amet ex."
-		},
-	];
+		var posts = [
+			{
+				"title":"Primer Post",
+				"date": "Publicado el " + moment().date() + " de "+ moment().format('MMMM') + " del "+ moment().format('YYYY'),
+				"content": "Etiam ultricies libero id porttitor ornare. Fusce hendrerit, dolor in efficitur euismod, dui lorem convallis quam, ac condimentum velit mauris vitae lectus. Vestibulum venenatis lacinia ante, id congue eros tincidunt in. Curabitur molestie nec augue non molestie. Fusce laoreet risus sit amet commodo fringilla. Pellentesque dapibus lorem et dolor tristique, id pulvinar urna lacinia. Pellentesque egestas neque vel arcu volutpat, malesuada hendrerit turpis semper. Sed vulputate purus nec rutrum pellentesque. Fusce ut tincidunt nulla, sed finibus turpis. Proin feugiat pulvinar ornare. Nulla ut sem ut ligula finibus pharetra. In lobortis ligula ac laoreet gravida. Nam vel orci lectus. Suspendisse potenti. Cras at nibh fringilla, vehicula felis sit amet, euismod augue. Vestibulum id diam et arcu suscipit pharetra non sit amet ex."
+			},
+			{
+				"title":"Segundo Post",
+				"date": "Publicado el " + moment().date() + " de "+ moment().format('MMMM') + " del "+ moment().format('YYYY'),
+				"content": "Etiam ultricies libero id porttitor ornare. Fusce hendrerit, dolor in efficitur euismod, dui lorem convallis quam, ac condimentum velit mauris vitae lectus. Vestibulum venenatis lacinia ante, id congue eros tincidunt in. Curabitur molestie nec augue non molestie. Fusce laoreet risus sit amet commodo fringilla. Pellentesque dapibus lorem et dolor tristique, id pulvinar urna lacinia. Pellentesque egestas neque vel arcu volutpat, malesuada hendrerit turpis semper. Sed vulputate purus nec rutrum pellentesque. Fusce ut tincidunt nulla, sed finibus turpis. Proin feugiat pulvinar ornare. Nulla ut sem ut ligula finibus pharetra. In lobortis ligula ac laoreet gravida. Nam vel orci lectus. Suspendisse potenti. Cras at nibh fringilla, vehicula felis sit amet, euismod augue. Vestibulum id diam et arcu suscipit pharetra non sit amet ex."
+			},
+			{
+				"title":"Tercer Post",
+				"date": "Publicado el " + moment().date() + " de "+ moment().format('MMMM') + " del "+ moment().format('YYYY'),
+				"content": "Etiam ultricies libero id porttitor ornare. Fusce hendrerit, dolor in efficitur euismod, dui lorem convallis quam, ac condimentum velit mauris vitae lectus. Vestibulum venenatis lacinia ante, id congue eros tincidunt in. Curabitur molestie nec augue non molestie. Fusce laoreet risus sit amet commodo fringilla. Pellentesque dapibus lorem et dolor tristique, id pulvinar urna lacinia. Pellentesque egestas neque vel arcu volutpat, malesuada hendrerit turpis semper. Sed vulputate purus nec rutrum pellentesque. Fusce ut tincidunt nulla, sed finibus turpis. Proin feugiat pulvinar ornare. Nulla ut sem ut ligula finibus pharetra. In lobortis ligula ac laoreet gravida. Nam vel orci lectus. Suspendisse potenti. Cras at nibh fringilla, vehicula felis sit amet, euismod augue. Vestibulum id diam et arcu suscipit pharetra non sit amet ex."
+			},
+		];
 
-	posts.forEach(post =>{
-		var template = `	
-		<article class="post">
-			<h2>${post.title}</h2>
-			<span class="date">${post.date}</span>
-			<p>${post.content}</p>
-			<a href="" class="button-more">Leer mas</a>
-		</article>`
-		
-		$('#posts').append(template);
-	});
+		posts.forEach(post =>{
+			var template = `	
+			<article class="post">
+				<h2>${post.title}</h2>
+				<span class="date">${post.date}</span>
+				<p>${post.content}</p>
+				<a href="" class="button-more">Leer mas</a>
+			</article>`
+			
+			$('#posts').append(template);
+		});
+	}
 
 
 // Selector theme
@@ -97,5 +100,8 @@ $(document).ready(function(){
 		location.reload();
 	})
 
-
+	if(window.location.href.indexOf('/about') > -1)
+	{
+		$('#accordion').accordion();
+	}
 })
